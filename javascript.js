16e54c002b4571bcec73c803f7588667351e434b
Kaya Arai-Yokoi <kaya.araiyokoi@gmail.com>
2023-08-19 20:44:41 +0200

    Create compute, submit, remove, input, findParenthesis function

--- javascript.js
+++ javascript.js
@@ -33,7 +33,78 @@ const add = function() {
     return answer
   };
 
-function submit() {
-    userInput = document.getElementById('userInput').value.replace(/ /g, '');
-    return userInput
+
+function input() {
+  let box = document.querySelector('#userInput');
+  if (this.textContent == 'Delete') return remove()
+  if (this.textContent == 'Clear') return box.value = null;
+  if (this.textContent == '=') return box.value += '=' + submit();
+  box.value += this.textContent;
+  return
 }
+
+function submit() {
+  let box = document.querySelector('#userInput').value.replace(/ /g, '').split(/([\(\)\+\-\*\/])/g).filter(item => item !== '');
+  return compute(box);
+}
+
+function remove() {
+  let box = document.querySelector('#userInput');
+  box.value =box.value.slice(0, -1);
+}
+
+function compute(expression) {
+  console.log(expression)
+  for (let i = 0; i < expression.length; i++) {
+    if (expression[i] === '(') {
+      findParenthesis(expression, i);
+
+    }
+  }
+  for (let i = 0; i < expression.length; i++) {
+    if (/[/*]/.test(expression[i])) {
+      console.log(expression[i-1]);
+      console.log(expression[i+1]);
+      let operator = expression[i];
+      let result = eval(`${expression[i-1]} ${operator} ${expression[i+1]}`);
+      expression.splice(i-1, 3, result);
+      console.log(expression);
+      i = i - 3;
+    }
+  }
+  for (let i = 0; i < expression.length; i++) {
+    if (expression[i] === '-' || expression[i] === '+') {
+      console.log(expression[i-1]);
+      console.log(expression[i+1]);
+      let operator = expression[i];
+      let result = eval(`${expression[i-1]} ${operator} ${expression[i+1]}`);
+      expression.splice(i-1, 3, result);
+      console.log(expression);
+      i = i - 3;
+    }
+  }
+  return expression
+
+}
+
+function findParenthesis(expression, number) {
+  let newExpression = [];
+  let j = number + 1;
+  let k = 0;
+  while (expression[j] !== ')') {
+    if (expression[j] === '(') {
+      findParenthesis(expression, j)
+    }
+    newExpression.push(expression[j]);
+    j++;
+    k++;
+    }
+  newExpression = compute(newExpression);
+  expression.splice(number, k+2, ...newExpression);
+  console.log(expression);
+  return newExpression
+}
+
+
+const buttons = document.querySelectorAll('button');
+buttons.forEach(button => button.addEventListener('click', input));
